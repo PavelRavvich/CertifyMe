0b54b976e1829e1105cd5bef29cc97d27347254c
Pavel Ravvich <pravvich@gmail.com>
2024-06-22 21:26:43 +0300

    upd prompt

--- conf.py
+++ conf.py
@@ -4,4 +4,6 @@ mqtt_push_topic = "5d9f651bff57b/esp32-cmd"
 mqtt_pull_topic = "5d9f651bff57b/laptop-cmd"
 
 max_tokens = 1024
-prompt = "Please describe the image."
+prompt = """
+Select the 1 corrent answer from the following options and privide only the number of the answer from 1.
+"""
